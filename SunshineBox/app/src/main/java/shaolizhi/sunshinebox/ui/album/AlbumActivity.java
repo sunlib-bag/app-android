992011d8d60510251696b27b3dc361b49f817a43
邵励治 <674940850@qq.com>
2018-03-07 19:14:56 +0800

    图集展示功能

--- SunshineBox/app/src/main/java/shaolizhi/sunshinebox/ui/album/AlbumActivity.java
+++ SunshineBox/app/src/main/java/shaolizhi/sunshinebox/ui/album/AlbumActivity.java
@@ -4,7 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.widget.Button;
+import android.util.Log;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
@@ -16,30 +16,49 @@ import java.util.Comparator;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.OnClick;
 import shaolizhi.sunshinebox.R;
 import shaolizhi.sunshinebox.ui.base.BaseActivity;
 import shaolizhi.sunshinebox.ui.course.Materials;
+import shaolizhi.sunshinebox.utils.ToastUtils;
 
 public class AlbumActivity extends BaseActivity {
 
     private final static String MATERIALS = "materials";
 
-    private List<File> data;
+    @OnClick(R.id.album_act_button3)
+    public void close() {
+        finish();
+    }
 
-    @BindView(R.id.album_act_button1)
-    Button lastImageButton;
+    @OnClick(R.id.album_act_button1)
+    public void clickLastImageButton() {
+        if (getPosition() == (files.size() - 1)) {
+            ToastUtils.showToast("已经是最后一张图片了!");
+        } else {
+            setPosition(getPosition() + 1);
+            showImageAndName(getPosition());
+        }
+    }
 
-    @BindView(R.id.album_act_button2)
-    Button nextImageButton;
+
+    @OnClick(R.id.album_act_button2)
+    public void clickNextImageButton() {
+        Log.e("AlbumActivity", "position" + getPosition());
+        if (getPosition() == 0) {
+            ToastUtils.showToast("已经是第一站图片了！");
+        } else {
+            setPosition(getPosition() - 1);
+            showImageAndName(getPosition());
+        }
+    }
 
     @BindView(R.id.album_act_imageview1)
     ImageView imageView;
 
     private List<File> files;
 
-    private List<String> names;
-
-    private int position;
+    private int position = 0;
 
     @Override
     protected int layoutId() {
@@ -48,13 +67,11 @@ public class AlbumActivity extends BaseActivity {
 
     @Override
     protected void created(Bundle bundle) {
-        data = new ArrayList<>();
         Materials materials = (Materials) getIntent().getSerializableExtra(MATERIALS);
         if (materials != null) {
             List<Materials.AlbumResource> resourceList = sortResourceList(materials);
             GetFileAndName getFileAndName = new GetFileAndName(resourceList).invoke();
             files = getFileAndName.getFiles();
-            names = getFileAndName.getNames();
         }
     }
 
@@ -72,7 +89,15 @@ public class AlbumActivity extends BaseActivity {
 
     @Override
     protected void resumed() {
-        showImageAndName(0);
+        showImageAndName(getPosition());
+    }
+
+    public int getPosition() {
+        return position;
+    }
+
+    public void setPosition(int position) {
+        this.position = position;
     }
 
     private void showImageAndName(int position) {
